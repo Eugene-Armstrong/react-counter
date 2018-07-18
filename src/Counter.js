6d7caa6d31e76296563e01c944ce62c2215284b4
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-18 17:17:42 +0800

    [Jeffery] generate list and count sum

--- src/Counter.js
+++ src/Counter.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 class Counter extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 };
+        this.state = { count: 0};
     }
     add = () => {
         this.setState({
             count: this.state.count + 1
         });
+        this.props.s(1);
     }
     remove = () => {
         this.setState({
             count: this.state.count - 1
         });
+        this.props.s(-1);
     }
     render() {
         return (
--- src/CounterGroup.js
+++ src/CounterGroup.js
@@ -3,12 +3,24 @@ import Counter from '../src/Counter'
 export default class CounterGroup extends Component {
     constructor(props) {
         super(props);
+        this.state = {sum : 0}
+    }
+    sum = (s) =>{
+        this.setState({sum:s+this.state.sum})
+    }
+    countAll(){
+        var buttonLists = [], counterNum = this.props.size, i;
+        for (i = 0; i < counterNum; i = i + 1) {
+            buttonLists.push(<Counter s={this.sum}/>);
+        }
+        return buttonLists;
     }
     render() {
-        var buttonLists = [], n = this.props.size, i;
-        for (i = 0; i < n; i = i + 1) {
-            buttonLists.push(<div><Counter /></div>);
-        }
-        return (<div>{buttonLists}</div>)
+        return (
+        <div>
+            {this.countAll()}
+            <span>Counting The Summary : {this.state.sum}</span>
+        </div>
+        )
     }
 }
